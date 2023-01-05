d3e016df798b233444866774add16343d23e6747
codefan <codefan@sina.com>
2023-01-05 19:23:36 +0800

    添加新课件

--- primaryc/all_lession.cpp
+++ primaryc/all_lession.cpp
@@ -1,11 +1,11 @@
 //声明要运行的函数 
-int unit13_main();
+int unit15_main();
 int unit100_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
 	//return unit100_main(argc, argv);
-	return unit13_main();
+	return unit15_main();
 }
 
 
--- primaryc/unit15.cpp
+++ primaryc/unit15.cpp
@@ -1,8 +1,22 @@
-#include <iostream>  
-#include <string>
+#include <iostream>
+#include <string> 
  
 using namespace std;
 int unit15_main()
 {
+	const string USER="wilbur", PSW="564937";
+	string user,psw;
+	cout <<"ÓÃ»§Ãû:";
+	cin>>user;
+	cout<<"ÃÜÂë:";
+	cin >>psw; 
+	if(user.compare(USER)==0) {
+		if(psw==PSW)
+			cout<<"Ç×°®µÄÐ¡ÅóÓÑ£¬»¶Ó­¹âÁÙ!";
+		else 
+			cout << "ÃÜÂë´íÎó£¡" ;
+	} else 
+		cout<<"ÓÃ»§Ãû´íÎó£¡" ; 
+	
 	return 0;
-}
+} 
