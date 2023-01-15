6ff1201da3bcc73ff10b48bbdbe22ff93de667e6
codefan <codefan@sina.com>
2023-01-15 15:40:11 +0800

    add unit 25

--- primaryc/all_lession.cpp
+++ primaryc/all_lession.cpp
@@ -1,10 +1,10 @@
 //声明要运行的函数
-int unit24_main();
+int unit25_main();
 
 int unit100_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
 	//return unit100_main(argc, argv);
-	return unit24_main();
+	return unit25_main();
 }
