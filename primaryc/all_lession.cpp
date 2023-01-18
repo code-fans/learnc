8965cddf21b8d170986fa0301ef865db17abc307
wilbur <yang_qh@126.com>
2023-01-18 19:05:36 +0800

    添加新课程unit29

--- primaryc/all_lession.cpp
+++ primaryc/all_lession.cpp
@@ -1,10 +1,10 @@
 //声明要运行的函数
-int unit27_main();
+int unit28_main();
 
 int unit100_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
 	//return unit100_main(argc, argv);
-	return unit27_main();
+	return unit28_main();
 }
---
+++ primaryc/unit28.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int unit28_main()
+{
+    int i,sum=0;
+    for(i=1;i<=100;i++)
+       sum+=i;
+    cout<<"1+2+3+4+...+100="<<sum;
+    return 0;
+}
