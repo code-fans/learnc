328812d1a4df98abd7a959cedaa1b9075f3f6973
wilbur <yang_qh@126.com>
2023-03-11 19:16:58 +0800

    添加24点计算

--- primaryc/all_lession.cpp
+++ primaryc/all_lession.cpp
@@ -1,10 +1,10 @@
 //声明要运行的函数
-int wilburChickenAndRabbit();
+int calc_main();
 int unit100_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
 	//return unit100_main(argc, argv);
 	//unit39_main();
-	return wilburChickenAndRabbit();
+	return calc_main();
 }
