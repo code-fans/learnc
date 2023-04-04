5e84d29cfa57a914b5144c7278057910ffd9bd2b
wilbur <yang_qh@126.com>
2023-04-04 21:49:33 +0800

    添加新课程51

--- primaryc/all_lession.cpp
+++ primaryc/all_lession.cpp
@@ -1,10 +1,10 @@
 //声明要运行的函数
-int primaryc();
+int unit58_main();
 int unit100_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
 	//return unit100_main(argc, argv);
 	//unit39_main();
-	return primaryc();
+	return unit58_main();
 }
