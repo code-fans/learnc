f36637732865fb045a9c287e65e48d43b463411f
codefan <codefan@sina.com>
2023-04-30 11:33:17 +0800

    添加排序算法

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
+
+void displayArray(int l, int ar[]){
+    for(int i=0; i<l; i++)
+        cout << setw(6) << ar[i];
+    cout << endl;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    freopen("srotdata.txt", "r", stdin);
+    int len;
+    cin >> len;
+    int a[len];
+
+    for(int i=0;i<len;i++)
+        cin>>a[i];
+
+
+
+
+    displayArray(len, a);
     return 0;
 }
