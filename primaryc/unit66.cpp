756b9641e50cf344523ed7107bf0b96b169bff41
qh15 <yang_qh@126.com>
2023-08-22 20:48:18 +0800

    添加文件说明

--- primaryc/unit66.cpp
+++ primaryc/unit66.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //加密 encrypt 
 string encrypt(string str, int key){
     string str2 = "";
-    for(int i=0; i<str.size(); i++){
+    for(int i=0; i<str.length(); i++){
         char ch = str[i];
         if(ch>='a' && ch<='z'){
             int offset = ch-'a' + key;
@@ -47,15 +47,16 @@ string decrypt(string str, int key){
 }
 
 int main(){
-    cout << "请输入一段明文";
+    cout << "这是一个凯撒密码加密和解密程序，请输入一段明文:"<<endl;
     string str2, str1;
     getline(cin, str1); //cin >> str1; //in input out output
     int key;
-    cout << "请输入一个大于1小于26的密码";
+    cout << "请输入一个大于1小于26的密码:"<<endl;
     cin >> key;
     str2 = encrypt(str1, key);
+    cout << "加密后的密文为:"<<endl;
     cout << str2 << endl;
-
+    cout << "再次解密后的明文:"<<endl;
     str2 = encrypt(str2, 26 - key);
     cout << str2 << endl;
     return 0;
