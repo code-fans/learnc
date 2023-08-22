7c3db2b6c804f3824b7f7af3d465a437d729788e
qh15 <yang_qh@126.com>
2023-08-22 20:06:16 +0800

    凯撒密码

--- primaryc/unit66.cpp
+++ primaryc/unit66.cpp
@@ -4,20 +4,59 @@
 
 using namespace std;
 
-int main(){
-    string str2,str1;
-    char s;
-    int i;
-    getline(cin, str1); //cin >> str1; //in input out output
-    for (i=0;i<str1.size();i++){
-        s=str1[i];
-        if ((s>='a'&&s<='z')||(s>='A'&&s<='Z')){
-            s-=3;
-            if((s>='Z'&&s<'a')||s<'A')
-                s+=26;
+//加密 encrypt 
+string encrypt(string str, int key){
+    string str2 = "";
+    for(int i=0; i<str.size(); i++){
+        char ch = str[i];
+        if(ch>='a' && ch<='z'){
+            int offset = ch-'a' + key;
+            if(offset >=26)
+                offset -=26;
+            ch = 'a' + offset;
+        } else if (ch>='A' && ch<='Z'){
+            int offset = ch-'A' + key;
+            if(offset >=26)
+                offset -=26;
+            ch = 'A' + offset;
         }
-        str2+=s;
+        str2 += ch;
     }
-    cout<<str2<<endl;
+    return str2;
+}
+
+//解密 decrypt
+string decrypt(string str, int key){
+    string str2 = "";
+    for(int i=0; i<str.size(); i++){
+        char ch = str[i];
+        if(ch>='a' && ch<='z'){
+            int offset = ch-'a' - key;
+            if(offset < 0)
+                offset +=26;
+            ch = 'a' + offset;
+        } else if (ch>='A' && ch<='Z'){
+            int offset = ch-'A' - key;
+            if(offset < 0)
+                offset +=26;
+            ch = 'A' + offset;
+        }
+        str2 += ch;
+    }
+    return str2;
+}
+
+int main(){
+    cout << "请输入一段明文";
+    string str2, str1;
+    getline(cin, str1); //cin >> str1; //in input out output
+    int key;
+    cout << "请输入一个大于1小于26的密码";
+    cin >> key;
+    str2 = encrypt(str1, key);
+    cout << str2 << endl;
+
+    str2 = encrypt(str2, 26 - key);
+    cout << str2 << endl;
     return 0;
 }
