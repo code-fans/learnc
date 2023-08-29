3e37c9acbebbedf6ee092c6f475feefa95010e7a
qh15 <yang_qh@126.com>
2023-08-29 16:59:24 +0800

    冒泡排序

--- algorithm/sort.cpp
+++ algorithm/sort.cpp
@@ -13,18 +13,14 @@ void displayArray(int data[], int len)
 // 选择排序
 void selectSort(int data[], int len)
 {
-    for (int i = 0; i < len - 1; i++)
-    {
+    for (int i = 0; i < len - 1; i++) {
         int min = i;
-        for (int j = i + 1; j < len; j++)
-        {
-            if (data[j] < data[min])
-            {
+        for (int j = i + 1; j < len; j++){
+            if (data[j] < data[min]){
                 min = j;
             }
         }
-        if (min != i)
-        {
+        if (min != i){
             int temp = data[min];
             data[min] = data[i];
             data[i] = temp;
@@ -35,6 +31,20 @@ void selectSort(int data[], int len)
 // 冒泡排序
 void bubbleSort(int data[], int len)
 {
+    int i,j,k;
+    for(i=0;i<len;i++)
+    {
+        j=data[i];
+        for(k=0;k<len-1;k++)
+            {
+                if (data[k]<data[k+1])
+                    {
+                        int temp = data[k];
+                        data[k] = data[k+1];
+                        data[k+1] = temp;  
+                    }
+            }
+    }
 }
 
 int main()
@@ -51,7 +61,7 @@ int main()
     // 显示排序前的数据
     displayArray(a, len);
     // 调用排序算法
-    selectSort(a, len);
+    bubbleSort(a, len);
     // 显示排序后的结果
     displayArray(a, len);
     return 0;
