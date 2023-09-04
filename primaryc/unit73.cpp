18f72611aa108b7010d9b653b9fe139f71ffddde
qh15 <yang_qh@126.com>
2023-09-04 19:34:00 +0800

    添加换行

--- primaryc/unit73.cpp
+++ primaryc/unit73.cpp
@@ -1,24 +1,26 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
+
 bool choushu(int n)
 {
-    while (n%2==0)
-        n/=2;
-    while (n%3==0)
-        n/=3;
-    while (n%5==0)
-        n/=5;
+    for(;n%2==0;n/=2);
+    for(;n%3==0;n/=3);
+    for(;n%5==0;n/=5);
     return(n==1);
 } 
+
 int main()
 {
     int num=0;
-    for(int i=1;i<=100;i++)
+    for(int i=1;i<=100000000;i++)
     {
         if(choushu(i)){
-            cout<<setw(6)<<i;
+            cout<<setw(11)<<i;
             num++;
+            if (num%5==0){
+                cout<<endl;
+            }
         }
     }
     cout<<endl<<"个数"<<num<<endl;
