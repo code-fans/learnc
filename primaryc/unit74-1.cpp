bf1cbd1282b6c7814236bb400cee9a8de3807233
qh15 <yang_qh@126.com>
2023-09-05 20:51:16 +0800

    添加课后习题

---
+++ primaryc/unit74-1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrimeNumber(int unm){
+    if(unm<2)
+        return false;
+    for (int i=2; i <=  sqrt(unm); i++){
+        if (unm%i==0)
+            return false;
+    }
+    return true;
+}
+
+bool PrimeNumber(int unm){
+    for(int i=10; i<unm; i*=10) {
+        if(i>10 && unm % i == unm % (i/10))
+            return false;
+        if(! isPrimeNumber(unm % i))
+            return false;
+    }
+    return isPrimeNumber(unm);
+}
+
+// 123 -> 321
+int reverse(int num){
+    int ans=0;
+    while (num>0)
+    {
+        ans =ans * 10 +  num % 10;
+        num /= 10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout << "请输入一个数字：";
+    cin >>  n;
+    cout << reverse(n) << endl;
+}
+
+int main2()
+{
+    int i;
+    for(i=1; i<=1000000; i++)
+    {
+        if (PrimeNumber(i))
+            cout<<i<<endl;
+    }
+    return 0;
+}
