7147bf9ed27f9c382029fded6243fce3915eb688
qh15 <yang_qh@126.com>
2023-09-08 19:04:49 +0800

    添加递归调用

--- primaryc/unit77.cpp
+++ primaryc/unit77.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
+
 int zuoye2(int n)
 {
-    if(n==7)
+
+    if(n==7){
+        cout << n << ": 好的，我是最后一个！" << endl;
         return 1;
-    else
-        return zuoye2(n+1)+1; 
+    }
+    cout << n << ": 第" << n+1 << "交作业" << endl;
+    int zy = zuoye2(n+1)+1; 
+    cout << n << ": 我收到了第" << n+1 <<"的作业"<< endl;
+    return zy;
 }
-int main2()
-{
-    cout<<zuoye2(1)<<endl;
-    return 0;
-}
-#include <iostream>
-using namespace std;
+
+
 int zuoye(int n)
 {
     if(n==1)
@@ -21,8 +22,9 @@ int zuoye(int n)
     else
         return zuoye(n-1)+2; 
 }
+
 int main()
 {
-    cout<<zuoye(10)<<endl;
+    cout<<zuoye2(1)<<endl;
     return 0;
 }
