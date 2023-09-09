58a00ceeb9f40e08b12b9e600ba71157d81c3242
qh15 <yang_qh@126.com>
2023-09-09 10:18:54 +0800

    整理代码

---
+++ primaryc/unit78.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 碾转相除法求最大公约数
+long long greatestCommonDivisor(long long num1, long long num2){
+
+}
+
+// 最小公倍数
+long long leastCommonMultiple(long long num1, long long num2){
+
+}
+
+int main(){
+
+
+    return 0;
+}
