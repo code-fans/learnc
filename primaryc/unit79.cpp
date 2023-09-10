6a0c682168e2d60d5f2bc40c2afb05a7e6c46c98
qh15 <yang_qh@126.com>
2023-09-10 09:37:00 +0800

    添加趣味题

--- primaryc/unit79.cpp
+++ primaryc/unit79.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // 计算斐波拉契数列
-long long fibonacciSequence(int n){
+long long fibonacciSequence(int n){    
     if (n<2)
         return 1;
     long long fn2 = 1, fn1 =1, fn;
@@ -30,6 +30,22 @@ long long fibonacciSequence(int n){
     else 
         return fibonacciSequence(n-1) + fibonacciSequence(n-2);
 
+    int i, fn, fn1, fn2;
+    if (n<2)
+        return 1;
+    fn = 1;
+    fn1 =1;
+    for(i=2;i<=n;i++){
+        fn2 = fn+fn1;
+        fn=fn+fn1;
+        fn1=fn-fn1;
+        fn=fn-fn1;
+        fn1=fn1+fn2;
+        fn2=fn1-fn2;
+        fn1=fn1-fn2;
+    }
+    return fn1;
+
 */
 /** 最优化求解
  * 有不同面额的硬币 9元、5元、2元 若干枚
@@ -37,14 +53,45 @@ long long fibonacciSequence(int n){
  * 求解如何使用最少枚数硬币达到对应的数值
  */
 int minCoinNumber(int yuan){
+    const int MAX_COIN = 100000;
+    if(yuan == 0)
+        return 0;
+    if(yuan == 1)
+        return MAX_COIN;
 
-    return 0;
+    if(yuan >=9){
+        int n9 = minCoinNumber(yuan-9);
+        int n5 = minCoinNumber(yuan-5);
+        int n2 = minCoinNumber(yuan-2);
+        if(n9 <= n5 && n9 <=n2){
+            return n9+1;
+        }
+        if(n5 < n9 && n5 <=n2){
+            return n5+1;
+        }
+        if(n2 < n5 && n2 < n9){
+            return n2+1;
+        }
+    } else if(yuan >=5){
+        int n5 = minCoinNumber(yuan-5);
+        int n2 = minCoinNumber(yuan-2);
+        if(n5  <= n2){
+            return n5+1;
+        } else {
+            return n2+1;
+        }
+    } else if(yuan >=2){
+        int n2 = minCoinNumber(yuan-2);
+        return n2+1;
+    }
+    return MAX_COIN;
 }
 
 int main(){
     int a;
     cin>>a;
-    cout<<fibonacciSequence(a)<<endl;
-    //cout<< minCoinNumber(a)<<endl;
+    //cout<<fibonacciSequence(a)<<endl;
+    int n = minCoinNumber(a);
+    cout<< "最少数量：" << n <<endl;
     return 0;
 }
