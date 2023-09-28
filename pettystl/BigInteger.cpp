e61fb27bf97b08066e899312819c6577dc3faf0e
qh15 <yang_qh@126.com>
2023-09-28 17:46:30 +0800

    优化代码

--- pettystl/BigInteger.cpp
+++ pettystl/BigInteger.cpp
@@ -81,13 +81,11 @@ namespace pettystl {
         }
         return xy;
     }
-
-
 }
 
 int main(){
     pettystl::BigInteger bint1, bint2, bint3;
-    std::cin >> bint1>> bint2;
+    std::cin >> bint1 >> bint2;
     bint3 = bint1 + bint2;
     std::cout << bint3 << std::endl;
     return 0;
--- trains/unit10.cpp
+++ trains/unit10.cpp
@@ -3,20 +3,16 @@ int main(){
     freopen("data/input.txt", "r"/* r read w write*/, stdin);
     int r,c,ans=0;
     scanf("%d%d",&r,&c);
-    int a[r*c], b[r*c];
+    int a[r*c*2];
 
-    for (int i = 0; i < r*c; i++)
+    for (int i = 0; i < r*c *2; i++)
     {
         scanf("%d", a+i);
-    }
-    for (int i = 0; i < r*c; i++)
-    {
-        scanf("%d", b+i);
     }
     
     for (int i = 0; i < r*c; i++)
     {
-        if (a[i]==b[i])
+        if (a[i]== a[r*c + i])
         {
             ans++;
         }
