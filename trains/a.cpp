c218486c2929be1eb8369e83a72c522a74ca68fb
codefan <codefan@sina.com>
2023-10-09 16:33:12 +0800

    添加训练代码

--- trains/a.cpp
+++ trains/a.cpp
@@ -1,30 +1,22 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
-
-int matrix[2][1001];
 int main()
 {
-    int row, temp;
-    cin >> row;
-    int * curRow, * preRow;
-    cin >> matrix[0][0];
-    for(int i=1; i<row; i++){
-        curRow = matrix[i%2];
-        preRow = matrix[(i+1)%2];
-        for(int j=0; j<=i; j++){
-            cin >> temp;
-            if(j==0)
-                curRow[j] = temp + preRow[j];
-            else
-                curRow[j] = temp + max(preRow[j], preRow[j-1]);
+    int a[7][2],ans=0,maxday;
+    for(int i=0;i<7;i++)
+        for(int m=0;m<2;m++)
+            cin>>a[i][m];
+    maxday=a[0][1]+a[0][0];
+    ans=1;
+    for (int i = 1; i < 7; i++)
+    {
+        if (maxday<a[i][0]+a[i][1])
+        {
+            maxday=a[i][0]+a[i][1];
+            ans=i;
         }
     }
-    int maxNum = curRow[0];
-    for(int i=1; i<row; i++){
-        if(maxNum < curRow[i])
-            maxNum = curRow[i];
-    }
-    cout << maxNum << endl;
-    return 0;
+    cout<<ans<<endl;
+    return 0;   
 }
