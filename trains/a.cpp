015fbc9eed656af301264f348587e494dac6b234
qh15 <yang_qh@126.com>
2023-10-12 19:14:47 +0800

    https://www.luogu.com.cn/problem/P1789

--- trains/a.cpp
+++ trains/a.cpp
@@ -2,28 +2,18 @@
 using namespace std;
 int a[300][300];
 
-void huoba( int x,int y)
+void huoba( int x,int y, bool ys)
 {
      for (int i = -2; i <=2; i++)
    {
         for (int j = -2; j <=2; j++)
         {
-            if( (i<0?-i:i) +(j<0?-j:j) <= 2 )
+            if(ys || (i<0?-i:i) +(j<0?-j:j) <= 2 )
                 a[x+i][y+j] = 1;
         } 
    }
 }
 
-void yingshi(int x,int y)
-{
-   for (int i = -2; i <=2; i++)
-   {
-        for (int j = -2; j <=2; j++)
-        {
-            a[x+i][y+j] = 1;
-        } 
-   }
-}
 
 int main()
 {
@@ -34,12 +24,12 @@ int main()
     for (int u = 0; u < c; u++)
     {
         cin>>x>>y;
-        huoba(x+2,y+2);
+        huoba(x+2,y+2, false);
     }
     for (int t = 0; t < d; t++)
     {
         cin>>x>>y;
-        yingshi(x+2,y+2);
+        huoba(x+2,y+2, true);
     }
     
     for (int t = 3; t < i+3; t++)
