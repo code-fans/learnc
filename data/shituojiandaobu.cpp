b8fa9f1a103e095850f93ffe8ba65163e40266b0
qh15 <yang_qh@126.com>
2024-01-05 21:05:21 +0800

    递归

--- data/shituojiandaobu.cpp
+++ data/shituojiandaobu.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
+#include <algorithm>
+#include <cstdio>
 #include <string>
+
 using namespace std;
-int shuiyingli(string str,string str1)
+int palouti(int a)
 {
-    if (str[0]==str[0])
-        cout<<"Tie"<<endl;
-    if ((str[0]=='R'&&str[0]=='S')||(str[0]=='S'&&str[0]=='p')||(str[0]=='P'&&str[0]=='R'))
-        cout<<"Player1"<<endl;
-    else
-        cout<<"Player2"<<endl;
-    return 0;
+    if(a<=1)
+        return 1;
+    return palouti(a-1)+palouti(a-2);
 }
 int main()
 {
-    int n;
-    cin>>n;
-    string str,str1;
-    for (int i = 0; i < n; i++){
-        cin>>str>>str1;
-        shuiyingli(str,str1);
+    int a;
+    while (cin>>a){
+        cout<<palouti(a)<<endl;
     }
     return 0;
 }
