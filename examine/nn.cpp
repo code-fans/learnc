db5dc84d2f3c6b11b1d64c28bdde8e07af329dd2
codefan <codefan@sina.com>
2024-01-09 15:40:49 +0800

    递归

--- examine/nn.cpp
+++ examine/nn.cpp
@@ -4,30 +4,45 @@
 #include <string>
 
 using namespace std;
-
+int ans,ans1;
+int def(int a, int b)
+{
+    while (b!=0){
+        if(a>=b)
+            a-=b;
+        if(a<b){
+            a=b+a;
+            b=a-b;
+            a=a-b;
+        }
+    }
+    return a;
+}
+void add(int &a,int &b)
+{
+    int j=ans1*b/def(ans1,b);
+    int g=j/ans1,k=j/b;
+    ans*=g;
+    ans1*=g;
+    a*=k;
+    b*=k;
+    ans=b+ans;
+    return ;
+}
 int main()
 {
-    int hang,m=2,n,k=0,l=0;
+    int n;
     cin>>n;
-    while (m<=n)
-    {
-        k=0;
-        while (n%m==0) {
-            k++;
-            n/=m;
-        }
-        if(l!=0&&k!=0)
-            cout<<'*';
-        if(k>=2){
-            cout<<m<<'^'<<k;
-            l++;
-        }
-        else if(k==1){
-            cout<<m;
-            l++;
-        }
-        m++;
-    }
+    int a,b,c,d;
+    char a1,b1;
+    cin>>a>>a1>>b;
+    ans=a;
+    ans1=b;
     
+    for (int i = 0; i < n-1; i++){
+        cin>>c>>b1>>d;
+        add(c,d);
+    }
+    cout<<ans<<'/'<<ans1;
     return 0;
 }
