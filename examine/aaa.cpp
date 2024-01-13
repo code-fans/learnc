1a3c53757681a576591e37fe4559a16c7da03cc6
qh15 <yang_qh@126.com>
2024-01-13 10:56:20 +0800

    堆栈

--- examine/aaa.cpp
+++ examine/aaa.cpp
@@ -1,21 +1,60 @@
 #include <iostream>
 #include <string>
+#include <cstdio>
 using namespace std;
-bool ni(string str,int a){
-    for (int i = 0; i < str.size(); i++)
-        if (str[i]==str[a]&&i!=a)
-            return false;
-    return true;
-}
 int main()
 {
-    string str;
-    cin>>str;
-    for (int i = 0; i < str.size(); i++)
-        if (ni(str,i)){
-            cout<<str[i]<<endl;
-            return 0;
+    int n,aaa;
+    int x,y,s;
+    char a[100][100]={{0}};
+    cin>>n >>s;
+    while(n>0 || s>0){
+        long long ans=0;
+        for (int i = 0; i < s; i++){
+            for (int j = 0; j < n; j++){
+                cin>>a[i][j];
+                if(a[i][j]=='@'){
+                    
+                    ans++;
+                }
+            }
         }
-    cout<<"no"<<endl;
+        
+    
+        while (1){
+            bool aaa=false;
+            for (int i = 0; i < s; i++){
+                for (int j = 0; j < n; j++){
+                    if(a[i][j]=='@'){
+                        if(i!=0&&a[i-1][j]=='.'){
+                            a[i-1][j]='@';
+                            ans++;
+                            aaa=true;
+                        }
+                        if(i!=s-1&&a[i+1][j]=='.'){
+                            a[i+1][j]='@';
+                            ans++;
+                            aaa=true;
+                        }
+                        if(j!=0&&a[i][j-1]=='.'){
+                            a[i][j-1]='@';
+                            ans++;
+                            aaa=true;
+                        }
+                        if(j!=n-1&&a[i][j+1]=='.'){
+                            a[i][j+1]='@';
+                            ans++;
+                            aaa=true;
+                        }
+                    }
+                }
+            }
+            if(!aaa)
+                break;
+        }
+        cout<<ans<<endl;
+        cin>>n >>s;
+    }
     return 0;
 }
+
