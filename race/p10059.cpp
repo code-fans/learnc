a5270255942b9342b781abc06a39247a0918d4a9
codefan <codefan@centit.com>
2024-01-15 14:41:01 +0800

    luogu竞赛

--- race/p10059.cpp
+++ race/p10059.cpp
@@ -1,46 +1,54 @@
 #include <algorithm>
 #include <iostream>
 #include <cstdio>
-#include <cmath>
+#include <deque>
 #include <string>
 using namespace std;
 
 long long qiuMinMax(const long long * a, int len, int k, int l){
     long long jiZhi [100001]={0};
-    int zjgs = len - l + 1;
-    for(int i=0; i<zjgs; i++){
-        long long maxA = a[i], minA=a[i];
-        for(int j=i+1; j< i+l; j++){
-            if(maxA < a[j]){
-                maxA = a[j];
-            }
-            if(minA > a[j]){
-                minA = a[j];
-            }
+    deque<int> minF;
+    deque<int> maxF;
+    for(int i=0; i<len; i++){
+        while (!minF.empty() && i - l >= minF.front()) minF.pop_front();
+		while (!minF.empty() && a[i] <= a[minF.back()]) minF.pop_back();
+		minF.push_back(i);
+
+        while (!maxF.empty() && i - l >= maxF.front()) maxF.pop_front();
+		while (!maxF.empty() && a[i] >= a[maxF.back()]) maxF.pop_back();
+		maxF.push_back(i);
+
+		if (i >= l-1){
+            jiZhi[i-l+1] = a[maxF.front()] - a[minF.front()];
         }
-        jiZhi[i] = maxA - minA;
     }
+    int zjgs = len - l + 1;
     sort(jiZhi, jiZhi+zjgs, greater<long long>());
     return jiZhi[k-1];
 }
 
 bool qiuMinMax2(const long long * a, int len, int k, int l, long long jz){
     int sub = 0;
-    int zjgs = len - l + 1;
-    for(int i=0; i<zjgs; i++){
-        long long maxA = a[i], minA=a[i];
-        for(int j=i+1; j< i+l; j++){
-            if(maxA < a[j]){
-                maxA = a[j];
+   
+    deque<int> minF;
+    deque<int> maxF;
+    for(int i=0; i<len; i++){
+        while (!minF.empty() && i - l>= minF.front()) minF.pop_front();
+		while (!minF.empty() && a[i] <= a[minF.back()]) minF.pop_back();
+		minF.push_back(i);
+
+        while (!maxF.empty() && i - l>= maxF.front()) maxF.pop_front();
+		while (!maxF.empty() && a[i] >= a[maxF.back()]) maxF.pop_back();
+		maxF.push_back(i);
+
+		if (i >= l-1){
+            long long cjz = a[maxF.front()] - a[minF.front()];
+            if(cjz >= jz){
+                sub++;
+                if(sub == k){
+                    return true;
+                }
             }
-            if(minA > a[j]){
-                minA = a[j];
-            }
-        }
-        if( jz <= maxA - minA)
-            sub ++;
-        if(sub == k){
-            return true;
         }
     }
     return false;
