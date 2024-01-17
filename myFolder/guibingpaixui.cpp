f3a8303efa8dfdd9d5c522bfb86ba9a124cf1129
qh15 <yang_qh@126.com>
2024-01-17 19:33:46 +0800

    归并排序

---
+++ myFolder/guibingpaixui.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+long long ans=0;
+int r[100000],a[100000];
+void msort(int s,int t){
+    if(s==t-1) return ;
+    int mid=(s+t)/2;
+    
+    msort(s,mid);
+    msort(mid,t);
+
+    int i=s,j=mid,k=s;
+
+    while (i<mid && j<t){
+        if (a[i]<=a[j]){
+            r[k]=a[i];
+            ans += j-mid;
+            k++;
+            i++;
+        }
+        else{
+            r[k]=a[j];
+            k++;
+            j++;
+        }
+    }
+    
+    while (i<mid){
+        r[k]=a[i];
+        ans+= t-mid;
+        i++;
+        k++;
+    }
+
+    while (j<t){
+        r[k]=a[j];
+        j++;
+        k++;
+    }
+    for (int i = s; i < t; i++)
+        a[i]=r[i];
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++){
+        cin>>a[i];
+    }
+    msort(0,n);
+    cout<<ans<<endl;
+    return 0;
+}
