47099b20b2a4a2ed4d9bc14975b322d4a61ec41a
codefan <codefan@sina.com>
2024-01-17 10:18:46 +0800

    归并排序

--- myFolder/quicksort.cpp
+++ myFolder/quicksort.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
 using namespace std;
-void qsort(int l,int r,int a[]){
-    int mid=a[(r+l)/2];
-    int i=l,j=r;
-    do
-    {
-        while(a[i]<mid) i++;
-        while(a[j]>mid) j--;
-        if(i<=j){
-            int p=a[i];
-            a[i]=a[j];
-            a[j]=p;
+void gsort(int s ,int t, int a[]){
+   
+    if(s==t-1) return ;
+    
+    int mid=(s+t)/2;
+    gsort(s, mid,a);
+    gsort(mid,t,a);
+    int i=s, j=mid,k=s;
+    int r[100];
+    while (i<mid && j<t){
+        if (a[i]<=a[j]){
+            r[k]=a[i];
+            k++;
             i++;
-            j--;
         }
-    } while (i<=j);
-    if(l<j) qsort(l,j,a);
-    if(i<r) qsort(i,r,a);
+        else{
+            r[k]=a[j];
+            j++;
+            k++;
+        }
+    }
+    while (i<mid)
+    {
+        r[k]=a[i];
+        k++;
+        i++;
+    }
+    while (j<t  )
+    {
+        r[k]=a[j];
+        k++;
+        j++;
+    }
+    for (int i = s; i < t; i++)
+        a[i]=r[i];
 }
 int main(){
     int n,a[1000]={0};
     cin>>n;
-    for (int i = 1; i <= n; i++){
+    for (int i = 0; i < n; i++){
         cin>>a[i];
     }
-    qsort(1,n,a);
-    for (int i = 1; i <= n; i++){
+    gsort(0, n, a);
+    for (int i = 0; i < n; i++){
         cout<<a[i]<<' ';
     }
     cout<<endl;
