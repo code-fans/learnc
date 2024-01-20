5d168a626be87ac7133908f33500136244a8ad89
qh15 <yang_qh@126.com>
2024-01-20 11:10:21 +0800

    luogu

--- race/ketket.cpp
+++ race/ketket.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
+#include <cmath>
+#include <cstdio>
 using namespace std;
-long long jcf(long long n){
-    return 1<<n;
-}
-long long ans(long long n,int p){
-    long long p2 = jcf(p);
-    long long p3=n & (p2-1);
-    p2=p2>>1;
-    if(p3>=p2)
-        return 0;
-    return p2-p3;
-}
 int main()
 {
-    long long a,b,c,x, sumX=0;
-    cin>>a>>b;
-    for (int i = 0; i < b; i++){
-        cin>>c;
-        long long ans1;
-        ans1=ans(a,c);
-        sumX+=ans1;
-        a+=ans1;
-    }
-    cout << sumX << endl;
+    long long  n;
+    cin>>n;
+    n*=n;
+    double ans=double(n)/100.0f;
+    printf("%0.0f\n",ceil(ans));
     return 0;
 }
