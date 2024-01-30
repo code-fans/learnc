43eebac90bebab7cb9f1260e5c8dfa94e4846e2b
qh15 <yang_qh@126.com>
2024-01-30 20:19:48 +0800

    提交

--- examine/a1.cpp
+++ examine/a1.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <algorithm>
 using namespace std;
-int ans[500]={0},a[300]={0},b[300]={0};
-int al,bl,ansl = 0;
-
-void jiechini(int jidejiecheng,int p)
-{
-    int t=0;
-    for (int i = 0; i < al; i++){
-        int y=a[i]*jidejiecheng+t + ans[i+p];
-        t= y/10;
-        ans[i+p]= y%10;
-    }
-    ansl= al+p;
-    if(t>0){
-        ans[ansl]=t;
-        ansl++;
-    }
-}
-
 int main()
 {
-    string str,str1;
-    cin>>str>>str1;
-    al=str.size();
-    bl=str1.size();
-    for (int i = 0; i < al; i++){
-        a[i]=str[al-1-i]-'0';
+    long long a=0,i=1;
+    long long ans=0;
+    while (a!=-1){
+        cout<<"输入1或2 输完请输 -1:";
+        cin>>a;
+        if(a==1)
+            ans+=i;
+        i*=2;
     }
-    for (int i = 0; i < bl; i++){
-        b[i]=str1[bl-1-i]-'0';
-    }
-    for (int i = 0; i < bl; i++){
-        jiechini(b[i], i);
-    }
-    for (int i = ansl-1; i >= 0; i--){
-        cout<<ans[i];
-    }
-    cout<<endl;
+    cout<<ans<<endl;
     return 0;
 }
