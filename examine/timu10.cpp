5e6bcadd7f8295f941d438846e529707c7ffc361
qh15 <yang_qh@126.com>
2024-02-27 17:12:59 +0800

    新课件

--- examine/timu10.cpp
+++ examine/timu10.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
+
 using namespace std;
-int main()
+int main1()
 {
-    int n;
-    cin>>n;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if((j==0)||(j==n-1)){
-                cout<<'|';
-            }
-            else if(i==(n/2)){
-                cout<<'-';
-            }
-            else{
-                cout<<'a';
-            }
-        }
-        cout<<endl;
+    int y,y1;
+    cin>>y>>y1;
+    if (y1==1||y1==3||y1==5||y1==7||y1==8||y1==10||y1==12){
+        cout<<31<<endl;
+    }
+    else if(y1!=2){
+        cout<<30<<endl;
+    }
+    else if ((y%4==0&&y%100!=0)||y%400==0){        
+        cout<<29<<endl;
+    }
+    else{
+        cout<<28<<endl;
     }
     return 0; 
+}
+
+int main()
+{
+    int mianji;
+    cin>>mianji;
+    int ans=0;
+    for (int i = 1; i * i <= mianji; i++){
+
+        if(mianji%i==0){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+    long long a=1024;
+    a*=a;
+    // a*=a;
+    cout<<endl<<a<<endl;
+    return 0;
 }
