6e8fed94ba0a68ea8476bc29095b41fd05e20e74
qh15 <yang_qh@126.com>
2024-03-02 20:22:48 +0800

    新题目

--- examine/timu17.cpp
+++ examine/timu17.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
-#include <cmath>
-#include <string>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    string str,str1;
+    string str;
     cin>>str;
     int i=0;
-    while (1){
-        int len=0;
-        int zhifu=0,shuzi=0,teshuzhifu=0,qita=0;
-        for (int j = 0; j < str.size()&&str[i]!=','; j++){
-            if(str[i]=='@'||str[i]=='&'||str[i]=='$'||str[i]=='#')
-                teshuzhifu++;
-            else if((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z'))
-                zhifu++;
+    int daxeishuzi=0,xiaoxieshuzi=0,teshuzifu=0,shuzi=0,kaito=0,jiewei,qita=0;
+    while (i<str.size()){
+        kaito=i;
+        while (i<str.size()&&str[i]!=','){
+            if(str[i]>='A'&&str[i]<='Z')
+                daxeishuzi=1;
+            else if(str[i]>='a'&&str[i]<='z')
+                xiaoxieshuzi=1;
             else if(str[i]>='0'&&str[i]<='9')
-                shuzi++;
-            else qita++;
+                shuzi=1;
+            else if(str[i]=='@'||str[i]=='#'||str[i]=='$'||str[i]=='!')
+                teshuzifu=1;
+            else qita=1;
+            i++;
         }
-        if(((zhifu+shuzi+teshuzhifu)>=6&&(zhifu+shuzi+teshuzhifu)<=12)&&qita==0&&)
-
+        jiewei=i;
+        if(!qita && daxeishuzi+xiaoxieshuzi+shuzi>1
+                   && teshuzifu && jiewei-kaito>=6 && jiewei-kaito<=12 ){
+            for (int j = kaito; j < jiewei; j++)
+                cout<<str[j];
+            cout<<endl;
+        }
         i++;
-        
     }
     return 0;
 }
+//seHJ12!@,sjdkffH$123,sdf!@&12HDHa!,123&^YUhg@!
