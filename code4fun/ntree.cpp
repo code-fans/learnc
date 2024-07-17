f5fe79c044276e1275bd024813497cfd882b06d7
codefan <codefan@centit.com>
2024-07-17 18:10:54 +0800

    树的表示法

--- code4fun/ntree.cpp
+++ code4fun/ntree.cpp
@@ -4,7 +4,7 @@ struct ntree{
     int l;
     int d;
     int r;
-}a[10000],di;
+} a[10000];
 
 int main()
 {
@@ -16,41 +16,31 @@ int main()
     a[1].l=-1;
     a[1].r=-1;
     for (int i = 2; i <= n; i++){
-       
         cin>>b;
         a[i].d=b;
         a[i].l=-1;
         a[i].r=-1;
-
-        int j=1,di;
-        if(a[j].d<b)
-            di=2;
-        else
-            di=0;
-        while ((a[j].r!=-1&&di==2)||(a[j].l!=-1&&di==0)){
-            if(b>a[j].d)
+        int j=1;
+        while(true){
+            if(b>a[j].d){ // >=
+                if(a[j].r == -1){
+                    a[j].r=i;
+                    break;
+                }
                 j = a[j].r;
-            else
-                j = a[j].l; 
-            if(b>=a[j].d){
-                di=2;
+            } else {
+                if(a[j].l == -1){
+                    a[j].l=i;
+                    break;
+                }
+                j = a[j].l;
             }
-            else{
-                di=0;
-            }
-        }
-        if(b>a[j].d)
-            a[j].r=i;
-        else
-            a[j].l=i;
+        } 
     }
 
     for (int i = 1; i <= n; i++){
-        cout<<a[i].l<<' '<<a[i].d<<' '<<a[i].r<<' ';
-        cout<<endl;
+        cout<<a[i].l<<' '<<a[i].d<<' '<<a[i].r<<endl;
     }
-    
-    cout<<endl;
     return 0;
 }
 /*
