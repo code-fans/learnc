ff2dcd7c1c0dd5cb42d42212ef986085de76cef0
codefan <codefan@centit.com>
2024-08-09 14:09:56 +0800

    修改bug

--- code4fun/testStl.cpp
+++ code4fun/testStl.cpp
@@ -8,7 +8,7 @@ int main() {
     int a[] = {1,1,2,3,3,3,3,3,5,6,7,8,9};
     int * e = lower_bound(a, a+13, 4);
     cout << *e << endl;
-    for(int * l = lower_bound(a, a+13, 4); l<upper_bound(a, a+13, 3); l++){
+    for(int * l = lower_bound(a, a+13, 4); l<upper_bound(a, a+13, 4); l++){
         cout << *l << endl;
     }
     return 0;
