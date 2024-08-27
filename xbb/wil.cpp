425915ee3104435ce7e58243d769baaf27778d6e
codefan <codefan@centit.com>
2024-08-27 15:00:47 +0800

    dddd

--- xbb/wil.cpp
+++ xbb/wil.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
 using namespace std;
-int n,e;
-int x[2000],y[2000],w[2000];
-int dad[2000];
-void quickort(int i,int j){
-    if (i>=j){
-        return; 
+
+int main()
+{
+    int k=0,num=0,x= 1,y=1,x1,y1;
+    cin>>x1>>y1;
+    int step[100]={0};
+    int dx[5]={1,2,2,1};
+    int dy[5]={-2,-1,1,2};
+    while (1)
+    {
+        // 走一步
+        x+=dx[k];
+        y+=dy[k];
+        step[num] = k;
+        num ++;
+        k=0;
+        //如果成功了
+        if(x == x1 && y== y1){
+            break;
+        }
+        if(x>x1 || y>y1 || y<1 || (y1-y)>2*(x1-x)){
+            //退一步
+            do{
+                num--;
+                if(num<0){
+                    printf("-1\n");
+                    return 0;
+                }
+                k=step[num];
+                x-=dx[k];
+                y-=dy[k];
+                k++;
+            }while(k>3);
+        }
     }
-    int m=i,n=j;
-    int k=w[(i+j)>>1];
-    while (m<=n){
-        
-    }
-    
-}
-int main(){
+
+    for (k = 0; k <  num; k++)
+        printf("%d ",step[k]+1);
+    printf("\n");
     return 0;
 }
