b75473d6bdca966215feefaf70bd0190f8cce72d
codefan <codefan@centit.com>
2024-09-05 08:53:09 +0800

    新题解

---
+++ xbb/m202016.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+bool nextJam( string & s, char e, int nl){
+    int j=nl-1;
+    while(j>=0 && s[j]== (e+j-nl+1)){
+        j--;
+    }
+    if(j<0) return false;
+    s[j] = s[j]+1;
+    for(j++;j<nl;j++){
+        s[j] = s[j-1]+1;
+    }
+    return true;
+}
+
+int main(){
+    int n,l,s;
+    cin >> n >> l >>s;
+    string str;
+    cin >> str;
+    char endChar = 'a' + l-1;
+    for(int i=0;i<5;i++){
+        if(! nextJam( str, endChar, s))
+            break;
+        cout << str << endl;
+    }
+    return 0;
+}
