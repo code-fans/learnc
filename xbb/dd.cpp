ef863c9ecc3f495fb6b67de9de5deffb20e61059
codefan <codefan@centit.com>
2024-11-07 16:13:07 +0800

    СÖíÖí

--- xbb/dd.cpp
+++ xbb/dd.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: codefan codefan@centit.com
+ * @Date: 2024-08-27 15:01:31
+ * @LastEditors: codefan codefan@centit.com
+ * @LastEditTime: 2024-11-07 16:12:59
+ * @FilePath: /learnc/xbb/dd.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <bits/stdc++.h>
 using namespace std;
 string a[100000];
